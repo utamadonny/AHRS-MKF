3dfd654603e3028635e0de94edfe158836173bb7
utamadonny <utamadonny10@gmail.com>
2021-07-04 17:59:18 +0700

    add mahony to test, move to new dir, start use bno

--- Arduino/bno/raw/raw.ino
+++ Arduino/bno/raw/raw.ino
@@ -2,6 +2,8 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
+#include <MadgwickAHRS.h>
+// #include <MahonyAHRS.h>
 
 /* This driver reads raw data from the BNO055
 
@@ -18,11 +20,16 @@
 */
 
 /* Set the delay between fresh samples */
-#define BNO055_SAMPLERATE_DELAY_MS (100)
+#define BNO055_SAMPLERATE_DELAY_MS (10)
+// !    Chooose Filter
+// Mahony filter;
+Madgwick filter;
+// ! Choose Filter
+float roll, pitch, yaw;
 
 // Check I2C device address and correct line below (by default address is 0x29 or 0x28)
 //                                   id, address
-Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28);
+Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x29);
 
 /**************************************************************************/
 /*
@@ -43,6 +50,9 @@ void setup()
   }
 
   delay(1000);
+  // ! Mahony tidak pakai sampling frekuensi
+  filter.begin(100);
+  // ! Un-comment if you use Madgwick Filter
 
   /* Display the current temperature */
   int8_t temp = bno.getTemp();
@@ -71,42 +81,80 @@ void loop()
   // - VECTOR_EULER         - degrees
   // - VECTOR_LINEARACCEL   - m/s^2
   // - VECTOR_GRAVITY       - m/s^2
+ 
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-
   /* Display the floating point data */
-  Serial.print("X: ");
+  // Serial.print("Orientation: ");
   Serial.print(euler.x());
-  Serial.print(" Y: ");
+  Serial.print(",");
   Serial.print(euler.y());
-  Serial.print(" Z: ");
+  Serial.print(",");
   Serial.print(euler.z());
-  Serial.print("\t\t");
 
-  
-  // Quaternion data
-  imu::Quaternion quat = bno.getQuat();
-  Serial.print("qW: ");
-  Serial.print(quat.w(), 4);
-  Serial.print(" qX: ");
-  Serial.print(quat.x(), 4);
-  Serial.print(" qY: ");
-  Serial.print(quat.y(), 4);
-  Serial.print(" qZ: ");
-  Serial.print(quat.z(), 4);
-  Serial.print("\t\t");
+ imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+ Serial.print(",");
+ Serial.print(acc.x());
+ Serial.print(",");
+ Serial.print(acc.y());
+ Serial.print(",");
+ Serial.print(acc.z());
 
+ imu::Vector<3> gyr = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+ Serial.print(",");
+ Serial.print(gyr.x());
+ Serial.print(",");
+ Serial.print(gyr.y());
+ Serial.print(",");
+ Serial.print(gyr.z());
+
+ imu::Vector<3> hag = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+ Serial.print(",");
+ Serial.print(hag.x());
+ Serial.print(",");
+ Serial.print(hag.y());
+ Serial.print(",");
+ Serial.print(hag.z());
+//  // Quaternion data/
+//  imu::Quaternion quat = bno.getQuat();
+//  Serial.print("qW: ");
+//  Serial.print(quat.w(), 4);
+//  Serial.print(" qX: ");
+//  Serial.print(quat.x(), 4);
+//  Serial.print(" qY: ");
+//  Serial.print(quat.y(), 4);
+//  Serial.print(" qZ: ");
+//  Serial.print(quat.z(), 4);
+//  Serial.print(" ");
+//! Choose 6  DoF or 9 DoF. Mahony only use 6 DoF
+  // filter.updateIMU(gyr.x(),gyr.y(),gyr.z(),acc.x(),acc.y(),acc.z());
+  filter.update(gyr.x(),gyr.y(),gyr.z(),acc.x(),acc.y(),acc.z(),hag.x(),hag.y(),hag.z());
+//! ---------------------------
+  roll=filter.getRoll();
+  pitch=filter.getPitch();
+  yaw=filter.getYaw();
+  Serial.print(",");
+  Serial.print(yaw);
+  Serial.print(",");
+  Serial.print(pitch);
+  Serial.print(",");
+  Serial.print(roll);
 
   /* Display calibration status for each sensor. */
   uint8_t system, gyro, accel, mag = 0;
   bno.getCalibration(&system, &gyro, &accel, &mag);
-  Serial.print("CALIBRATION: Sys=");
+//  Serial.print("CALIBRATION: Sys=");
+  Serial.print(",");
   Serial.print(system, DEC);
-  Serial.print(" Gyro=");
+//  Serial.print(" Gyro=");
+  Serial.print(",");
   Serial.print(gyro, DEC);
-  Serial.print(" Accel=");
+//  Serial.print(" Accel=");
+  Serial.print(",");
   Serial.print(accel, DEC);
-  Serial.print(" Mag=");
+    Serial.print(",");
+//  Serial.print(" Mag=");
   Serial.println(mag, DEC);
 
   delay(BNO055_SAMPLERATE_DELAY_MS);
 }
+
