5c0d3d032c808e5cb4ee4fb12635cfd4e474e2cb
utamadonny <utamadonny10@gmail.com>
2021-11-03 11:50:44 +0700

    add fig

--- madgwick-algorithm_matlab/ExampleScript.m
+++ madgwick-algorithm_matlab/ExampleScript.m
@@ -24,31 +24,28 @@ addpath('quaternion_library');      % include quaternion library
 % addpath('Data');                  % add data
 %% Clear 
 close all;                          % close all figures
-% clear;                              % clear all variables
-% clc;                                % clear the command terminal
+clear;                              % clear all variables
+clc;                                % clear the command terminal
 % set(0,'DefaultFigureWindowStyle','docked')
 % set(0,'DefaultFigureWindowStyle','normal')
 %% Debug#0 export csv test
-file = 'F:\Git Ubuntu\BelajarJulia\ahrs\2d250v1.csv';
-Time = readmatrix(file ,'range','A:A','OutputType','datetime');
-reuler = readmatrix(file ,'range','B:D','OutputType','double');
-Accelerometer = readmatrix(file ,'range','E:G','OutputType','double');
-Gyroscope = readmatrix(file ,'range','H:J','OutputType','double');
-Magnetometer = readmatrix(file ,'range','K:M','OutputType','double');
-aeuler = readmatrix(file ,'range','N:P','OutputType','double');
-time = readmatrix(file ,'range','Y:Y','OutputType','double');
+% file = 'F:\Git Ubuntu\BelajarJulia\ahrs\2d250v1.csv';
+% Time = readmatrix(file ,'range','A:A','OutputType','datetime');
+% reuler = readmatrix(file ,'range','B:D','OutputType','double');
+% Accelerometer = readmatrix(file ,'range','E:G','OutputType','double');
+% Gyroscope = readmatrix(file ,'range','H:J','OutputType','double');
+% Magnetometer = readmatrix(file ,'range','K:M','OutputType','double');
+% aeuler = readmatrix(file ,'range','N:P','OutputType','double');
+% time = readmatrix(file ,'range','Y:Y','OutputType','double');
 %% (1a) Import and plot sensor data
 % load("Nilai Beta");          |       RMSE             | Learning
                           %Beta| Yaw    | Pitch | Roll  | Time
-% load('Data/5v.mat');    %  5 | 1.7704 |0.1025 |0.2390 | 5.5
-% load('Data/2v.mat');    %  2 | 0.5802 |0.1795 |0.1252 | 22
-% load('Data/2v0.mat');   %  2 | 0.6981	|0.1878 |0.1182 | 2.2
+% load('Data2/5d80v.mat');    %  5 | 1.7704 |0.1025 |0.2390 | 5.1
+% load('Data/2v.mat');    %  2 | 0.5802 |0.1795 |0.1252 | 21
+% load('Data/2v0.mat');   %  2 | 0.6981	|0.1878 |0.1182 | 2.329
 % load('Data/2v1.mat');   %  2 | 0.6615	|0.1509 |0.1189 | 2.5
 % load('Data/2v2.mat');   %  2 | 0.8225 |0.1575 |0.1158 | 2.3
-% save('Data/2v3.mat');   %  2 | 0.7945	|0.1159 |0.0598 |
-% save('Data/2v4.mat');   %  2 | 0.4829	|0.1157 |0.0586 |
-% save('Data/2v5.mat');   %  2 | 0.3933	|0.0945 |0.0608 | 
-% load('Data/1v.mat');    %  1 | 0.6149	|0.2388 |0.2217 | 29
+
 % load('Data/1v0.mat');   %  1 | 0.7743	|0.1031 |0.0901 | 20 
 % load('Data/1v1.mat');   %  1 | 0.7632	|0.0960 |0.0868 | 22
 % load('Data/1v2.mat');   %  1 | 0.4812	|0.0657 |0.0917 | 20
@@ -61,15 +58,16 @@ time = readmatrix(file ,'range','Y:Y','OutputType','double');
 % load('Data/0c5v2.mat'); % 0.5| 0.9983 |0.1726 |0.0542 | 65
 % load('Data/0c5v3.mat'); % 0.5| 0.7166 |0.0854 |0.0533 | 10
 % load('Data/0c5v4.mat'); % 0.5| 0.2174 |0.1159 |0.0382 | 72
-% load('Data/0c5v5.mat'); % 0.5| 0.5318 |0.1283 |0.0297 | 95
+% load('Data2/0c5d10v.mat');     % 0.5| 0.5318 |0.1283 |0.0297 | 96.216
+% load('Data2/0c5d125v.mat');    %  1 | 0.6149 |0.2388 |0.2217 | 29.282
 % load('Data/0c3v.mat');  % 0.3| 1.1805 |0.3711 |0.0646 | 108
 % load('Data/0c3v0.mat'); % 0.3| 0.3291 |0.1873 |0.1282 | 109
 % load('Data/0c3v1.mat'); % 0.3| 0.2668 |0.3145 |0.3635 | 130
 % load('Data/0c3v2.mat'); % 0.3| 0.1829 |0.2612 |0.2934 | 131
-% load('Data/0c3v3.mat'); % 0.3| 0.3759 |0.3245 |0.4334 | 120
-% load('Data/0c3v4.mat'); % 0.3| 0.2295 |0.0993 |0.2221 | 117
+% load('Data/0c3v3.mat'); % 0.3| 0.3759 |0.3245 |0.4334 | 116.982
+% load('Data/0c3v4.mat'); % 0.3| 0.2295 |0.0993 |0.2221 | 117 
 % load('Data/0c3v5.mat'); % 0.3| 1.0563 |0.3481 |0.0790 | 112
-% load('Data/0c1v.mat');  % 0.1| 0.5294 |0.2695 |0.0706 | 270
+% load('Data2/0c1d270v.mat');    % 0.1| 0.5294 |0.2695 |0.0706 | 256.58
 % load('Data/0c1v0.mat'); % 0.1| 0.0610 |0.1356 |0.0579 | 416
 
 % load('Data/2021-09-08 17-19-10'); %Beta = 0.05 ga dapet
@@ -80,70 +78,72 @@ time = readmatrix(file ,'range','Y:Y','OutputType','double');
 
 % KESIMPULAN : Learning time vary karena derajat akhir/goal nya
 %% (1b) Import and plot sensor data  (Semua di 100derajat yaw)
-% load("Nilai Beta");           |       RMSE             | Learning
+% load("Nilai Beta");           |       RMSE             | Learningx`x``
                                %Beta | Yaw    | Pitch | Roll  | Time
-% save('Data2/5d100v.mat');    %  5 | 0.7674 |0.1086 |0.2211 |   5
-% save('Data2/5d100v0.mat');   %  5 | 0.8946 |0.1176 |0.2417 |   5
-% load('Data2/5d100v1.mat');   %  5 | 0.8390 |0.1241 |0.2539 |   5
+Beta='0c1';
+X=string(Beta)+'d270v';
+load ('Data2/'+string(X)+'.mat')
+% load('Data2/5d100v.mat');    %  5 | 0.7674 |0.1086 |0.2211 |   4.28
+% load('Data2/5d100v0.mat');   %  5 | 0.8946 |0.1176 |0.2417 |   5.7 
+% load('Data2/5d100v1.mat');   %  5 | 0.8399 |0.1244 |0.2547 |   4.3
 
-% load('Data2/5d150v.mat');    %  5 | 0.4237 |0.2530 |0.2043 |   1
+% load('Data2/5d150v.mat');    %  5 | 0.4237 |0.2530 |0.2043 |   1.441
 % load('Data2/5d150v0.mat');   %  5 | 0.5498 |0.2815 |0.2315 |   1.5
 % load('Data2/5d150v1.mat');   %  5 | 0.7935 |0.2894 |0.1823 |   1.6
 
-% load('Data2/5d250v.mat');    %  5 | 1.9919 |0.1518 |0.2826 |   3.6
-% load('Data2/5d250v0.mat');   %  5 | 0.7389 |0.1488 |0.2761 |   4.2
+% load('Data2/5d250v.mat');    %  5 | 1.9919 |0.1518 |0.2826 |   3.816
+% load('Data2/5d250v0.mat');   %  5 | 0.7389 |0.1488 |0.2761 |   3.882
 % load('Data2/5d250v1.mat');   %  5 | 0.7395 |0.1460 |0.2780 |   3.6
 
-% load('Data2/2d100v.mat');    %  2 | 1.1868 |0.1599 |0.2730 |  12
-% load('Data2/2d100v0.mat');   %  2 | 1.2675 |0.1645 |0.2739 |  12
-% load('Data2/2d100v1.mat');   %  2 | 1.0851 |0.1694 |0.2712 |  10
+% load('Data2/2d100v.mat');    %  2 | 1.1868 |0.1599 |0.2730 |  10.583
+% load('Data2/2d100v0.mat');   %  2 | 1.2675 |0.1645 |0.2739 |  10.528
+% load('Data2/2d100v1.mat');   %  2 | 1.0851 |0.1694 |0.2712 |  10.62
 
-% load('Data2/2d150v.mat');    %  2 | 0.4700 |0.1554 |0.1181 |   4
-% load('Data2/2d150v0.mat');   %  2 | 0.5647 |0.1569 |0.1208 |   4
-% load('Data2/2d150v1.mat');   %  2 | 0.6189 |0.1543 |0.1448 |   4
+% load('Data2/2d150v.mat');    %  2 | 0.4700 |0.1554 |0.1181 |   3.822
+% load('Data2/2d150v0.mat');   %  2 | 0.5647 |0.1569 |0.1208 |   3.731
+% load('Data2/2d150v1.mat');   %  2 | 0.6189 |0.1543 |0.1448 |   3.757
 
-% load('Data2/2d250v.mat');    %  2 | 1.1767 |0.0805 |0.1504 |  10
-% load('Data2/2d250v0.mat');   %  2 | 1.4208 |0.0830 |0.1464 |   9.7
-% load('Data2/2d250v1.mat');   %  2 | 1.2257 |0.0909 |0.1450 |   9.8
+% load('Data2/2d250v.mat');    %  2 | 1.1767 |0.0805 |0.1504 |   9.486
+% load('Data2/2d250v0.mat');   %  2 | 1.4208 |0.0830 |0.1464 |   9.444
+% load('Data2/2d250v1.mat');   %  2 | 1.2257 |0.0909 |0.1450 |   9.502
 
-% load('Data2/1d100v.mat');    %  1 | 0.8913 |0.0592 |0.1051 |  22
-% load('Data2/1d100v0.mat');   %  1 | 0.5818 |0.0711 |0.1401 |  21
-% load('Data2/1d100v1.mat');   %  1 | 0.8914 |0.0957 |0.1702 |  21
+% load('Data2/1d100v.mat');    %  1 | 0.8913 |0.0592 |0.1051 |  21.11
+% load('Data2/1d100v0.mat');   %  1 | 0.5818 |0.0711 |0.1401 |  21.146
+% load('Data2/1d100v1.mat');   %  1 | 0.8914 |0.0957 |0.1702 |  20.89
 
-% load('Data2/1d150v.mat');    %  1 | 0.7892 |0.0800 |0.0730 |   8
-% load('Data2/1d150v0.mat');   %  1 | 0.7831 |0.0811 |0.0676 |   6
-% load('Data2/1d150v1.mat');   %  1 | 0.6479 |0.0800 |0.0650 |   7
+% load('Data2/1d150v.mat');    %  1 | 0.7892 |0.0800 |0.0730 |   7.15
+% load('Data2/1d150v0.mat');   %  1 | 0.7831 |0.0811 |0.0676 |   7.377
+% load('Data2/1d150v1.mat');   %  1 | 0.6479 |0.0800 |0.0650 |   7.042
 
-% load('Data2/1d250v.mat');    % 0.5| 0.3262 |0.1179 |0.1106 |  20
-% load('Data2/1d250v0.mat');   % 0.5| 0.2914 |0.1003 |0.1037 |  20
-% load('Data2/1d250v1.mat');   % 0.5| 0.3176 |0.1136 |0.1057 |  20
+% load('Data2/1d250v.mat');    %  1 | 0.3262 |0.1179 |0.1106 |  20.63
+% load('Data2/1d250v0.mat');   %  1 | 0.2914 |0.1003 |0.1037 |  19.758
+% load('Data2/1d250v1.mat');   %  1 | 0.3176 |0.1136 |0.1057 |  19.644
 
 % load('Data2/0c5d100v.mat');  % 0.5| 0.5620 |0.0572 |0.0856 |  41
-% load('Data2/0c5d100v0.mat'); % 0.5| 0.3617 |0.0797 |0.1247 |  41
-% load('Data2/0c5d100v1.mat'); % 0.5| 0.6390 |0.0830 |0.1108 |  41
+% load('Data2/0c5d100v0.mat'); % 0.5| 0.3617 |0.0797 |0.1247 |  41.092
+% load('Data2/0c5d100v1.mat'); % 0.5| 0.6390 |0.0830 |0.1108 |  41.116
 
-% load('Data2/0c5d150v.mat');  % 0.5| 0.2675 |0.0607 |0.0615 |  16
-% load('Data2/0c5d150v0.mat'); % 0.5| 0.2796 |0.0540 |0.0423 |  15
-% load('Data2/0c5d150v1.mat'); % 0.5| 0.2223 |0.0503 |0.0391 |  15
+% load('Data2/0c5d150v.mat');  % 0.5| 0.2675 |0.0607 |0.0615 |  15.167
+% load('Data2/0c5d150v0.mat'); % 0.5| 0.2796 |0.0540 |0.0423 |  14.562
+% load('Data2/0c5d150v1.mat'); % 0.5| 0.2223 |0.0503 |0.0391 |  14.963
 
-% load('Data2/0c5d250v.mat');  % 0.5| 0.5192 |0.0713 |0.0618 |  38
-% load('Data2/0c5d250v0.mat'); % 0.5| 0.2823 |0.0837 |0.0599 |  37
+% load('Data2/0c5d250v.mat');  % 0.5| 0.5192 |0.0713 |0.0618 |  37.387
+% load('Data2/0c5d250v0.mat'); % 0.5| 0.2823 |0.0837 |0.0599 |  37.901
 % load('Data2/0c5d250v1.mat'); % 0.5| 0.3649 |0.0772 |0.0591 |  36
 
 % load('Data2/0c3d100v.mat');  % 0.3| 1.1805 |0.3711 |0.0646 | 
 % load('Data2/0c3d100v0.mat'); % 0.3| 0.3291 |0.1873 |0.1282 | 
 % load('Data2/0c3d100v1.mat'); % 0.3| 0.2668 |0.3145 |0.3635 | 
 
-% load('Data2/0c1d100v.mat');  % 0.1| 0.8591 |0.0529 |0.0560 | 220
-% load('Data2/0c1d100v0.mat'); % 0.1| 1.2564 |0.0499 |0.1374 | 220
-% laod('Data2/0c1d100v1.mat'); % 0.1| 1.1304 |0.0461 |0.1239 | 220
+% load('Data2/0c1d100v.mat');  % 0.1| 0.8591 |0.0529 |0.0560 | 211.916
+% load('Data2/0c1d100v0.mat'); % 0.1| 1.2564 |0.0499 |0.1374 | 212.556
+% load('Data2/0c1d100v1.mat'); % 0.1| 1.1304 |0.0461 |0.1239 | 211.612
 %% Set Parameter
-A='Time (s) \beta=2 Data2=2d250v1';
-
+A='Time (s) \beta='+string(Beta)+' Data2='+string(X);
 %% Set Parameter
-sta = 800; stb = length(time);
-SamplePeriode = 100;
-BetaQ= 0.2;
+% sta = 800; stb = length(time);
+% SamplePeriode = 100;
+% BetaQ= 0.5;
 % save('Data\aaaaa.mat');
 % Kp= 10; Ki=0;
 %% Debug#1
@@ -178,7 +178,6 @@ BetaQ= 0.2;
 % %     ,'arduino\phi', 'arduino\theta', 'arduino\psi'...
 % %     ,'real\phi', 'real\theta', 'real\psi');
 % hold off;
-
 %% (2) Preprocess to NED
 %  filter.update(gyr.y(),gyr.x(),-gyr.z(),-acc.y(),-acc.x(),acc.z(),-hag.y(),-hag.x(),hag.z());
 %  working
@@ -199,38 +198,39 @@ Magnetometer1 = tuker(Magnetometer1,1,2);
 Accelerometer1=tuker(Accelerometer1,1,2);
 Gyroscope1=tuker(Gyroscope1,1,2);
 %% (3) Plot Sensor Data
-% figure('Name', 'Sensor Data');
-% axis(1) = subplot(3,1,1);
-% hold on;
-% plot(time, Gyroscope(:,1), 'r');
-% plot(time, Gyroscope(:,2), 'g');
-% plot(time, Gyroscope(:,3), 'b');
-% legend('X', 'Y', 'Z');
-% xlabel('Time (s)');
-% ylabel('Angular rate (deg/s)');
-% title('Gyroscope');
-% hold off;
-% axis(2) = subplot(3,1,2);
-% hold on;
-% plot(time, Accelerometer(:,1), 'r');
-% plot(time, Accelerometer(:,2), 'g');
-% plot(time, Accelerometer(:,3), 'b');
-% legend('X', 'Y', 'Z');
-% xlabel('Time (s)');
-% ylabel('Acceleration (g)');
-% title('Accelerometer');
-% hold off;
-% axis(3) = subplot(3,1,3);
-% hold on;
-% plot(time, Magnetometer(:,1), 'r');
-% plot(time, Magnetometer(:,2), 'g');
-% plot(time, Magnetometer(:,3), 'b');
-% legend('X', 'Y', 'Z');
-% xlabel('Time (s)');
-% ylabel('Flux (G)');
-% title('Magnetometer');
-% hold off;
-% linkaxes(axis, 'x');
+figure('Name', 'Sensor Data');
+axis(1) = subplot(3,1,1);
+hold on;
+plot(time, Gyroscope(:,1), 'r');
+plot(time, Gyroscope(:,2), 'g');
+plot(time, Gyroscope(:,3), 'b');
+legend('X', 'Y', 'Z');
+xlabel('Time (s)');
+ylabel('Angular rate (deg/s)');
+title('Gyroscope');
+hold off;
+axis(2) = subplot(3,1,2);
+hold on;
+plot(time, Accelerometer(:,1), 'r');
+plot(time, Accelerometer(:,2), 'g');
+plot(time, Accelerometer(:,3), 'b');
+legend('X', 'Y', 'Z');
+xlabel('Time (s)');
+ylabel('Acceleration (g)');
+title('Accelerometer');
+hold off;
+axis(3) = subplot(3,1,3);
+hold on;
+plot(time, Magnetometer(:,1), 'r');
+plot(time, Magnetometer(:,2), 'g');
+plot(time, Magnetometer(:,3), 'b');
+legend('X', 'Y', 'Z');
+xlabel('Time (s)');
+ylabel('Flux (G)');
+title('Magnetometer');
+hold off;
+linkaxes(axis, 'x');
+savefig('Image/input/'+string(X)+'.fig');
 % % fig2plotly()
 
 %% (4a) Process sensor data through algorithm
@@ -309,20 +309,20 @@ amser
 % max(euler(sta:length(euler),1))  % For Scale Max range
 
 %% Save to CSV
-save('dummy.mat');
-fd= load('dummy.mat');
-T1=array2table(fd.euler);
-T1.Properties.VariableNames(1:3) = {'myaw','mpitch','mroll'};
-T2=array2table(fd.reuler);
-T2.Properties.VariableNames(1:3) = {'yaw','pitch','roll'};
-T3=array2table(fd.aeuler);
-T3.Properties.VariableNames(1:3) = {'ayaw','apitch','aroll'};
-timet=array2table(time);
-% writematrix( 
-Nums=[T2 T3 T1 timet];
-writetable(Nums,'2021-07-02 08-47-47M1.csv');
-% Nums=[reuler meuler euler time];
-% writematrix(Nums,'dummy.csv')
-% csvwrite('2021-07-16 21-43-46M.csv',filedata.reuler,filedata.meuler,filedata.euler)
-% save('Data/0c3v.mat');
+% save('dummy.mat');
+% fd= load('dummy.mat');
+% T1=array2table(fd.euler);
+% T1.Properties.VariableNames(1:3) = {'myaw','mpitch','mroll'};
+% T2=array2table(fd.reuler);
+% T2.Properties.VariableNames(1:3) = {'yaw','pitch','roll'};
+% T3=array2table(fd.aeuler);
+% T3.Properties.VariableNames(1:3) = {'ayaw','apitch','aroll'};
+% timet=array2table(time);
+% % writematrix( 
+% Nums=[T2 T3 T1 timet];
+% writetable(Nums,'2021-07-02 08-47-47M1.csv');
+% % Nums=[reuler meuler euler time];
+% % writematrix(Nums,'dummy.csv')
+% % csvwrite('2021-07-16 21-43-46M.csv',filedata.reuler,filedata.meuler,filedata.euler)
+% % save('Data/0c3v.mat');
 %%
